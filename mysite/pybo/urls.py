b493b0576040e3c8b4bc28c47054a85634a5e6ad
도형준 <105966480+dorae222@users.noreply.github.com>
2023-02-05 03:47:53 +0900

    update

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
-    path('test_page', views.test)
+    path('', views.index, name='index'),
+    #path('test_page', views.test),
+    path('<int:question_id>', views.detail, name='detail')
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Question
 
 # Create your views here.
 
-def index(requet):
-    return HttpResponse("pybo 페이지에 접근하셨습니다.")
+def index(request):
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html',context)
 
 def test(request):
     return HttpResponse('<h1>test page에 접근하셨습니다<\h1>')
+
+def detail(request,question_id):
+    '''
+    pybo 내용 출력
+    '''
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    context = {'question':question}
+    return render(request,'pybo/question_detail.html',context)
